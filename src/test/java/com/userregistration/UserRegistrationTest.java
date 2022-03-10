9d6525405adfc030bc22e3d745e6a59af8f13cd7
karthikbenki <karthikmc007@gmail.com>
2022-03-10 21:24:43 +0530

    Added: Valid First name regex

---
+++ src/test/java/com/userregistration/UserRegistrationTest.java
@@ -0,0 +1,12 @@
+package com.userregistration;
+
+import org.junit.jupiter.api.Test;
+
+public class UserRegistrationTest {
+
+    @Test
+    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        userRegistration.validateFirstName("Karthik");
+    }
+}
