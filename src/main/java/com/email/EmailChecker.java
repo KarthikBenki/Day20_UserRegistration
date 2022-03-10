f3d80010d475a41172ce4ab6bca1c75acfb1b980
karthikbenki <karthikmc007@gmail.com>
2022-03-11 05:08:57 +0530

    Added: Parameterized juit test for email checking

---
+++ src/main/java/com/email/EmailChecker.java
@@ -0,0 +1,11 @@
+package com.email;
+
+import java.util.regex.Pattern;
+
+public class EmailChecker {
+    private static final String EMAIL_PATTERN = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+    public boolean validateEmail(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        return pattern.matches(EMAIL_PATTERN,email);
+    }
+}
